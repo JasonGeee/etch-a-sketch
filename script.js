0eefaccf96236e8801cf01dfddee6a3702111ef8
Jason Gutierrez <jasongeee@yahoo.com>
2022-02-11 20:47:16 -0800

    Made changes, but starting a new branch since I am stuck and want to try a new approach

--- script.js
+++ script.js
@@ -9,27 +9,35 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
-
-function colorChange() {
-    // grid.style.backgroundColor = "red";
+//Creates a new grid on the webpage
+function makeGrid() {
+  let newGrid = prompt("Enter a number for a new grid from 0 - 100!", 16);
+  if (newGrid > 0 && newGrid < 101) {
+    makeRows(newGrid, newGrid);
+  } else {
+      makeGrid();
+    }
 }
 
 //Event listener for hovering over grid to change color
 container.addEventListener('mouseover', (e) => {
-    if (e.target.className == "grid-item") {
-        e.target.style.backgroundColor = "black";
-    }
+  console.log(e.target.value);
+  if (e.target.className == "grid-item") {
+    e.target.style.backgroundColor = "black";
+  }
 });
 
+//Event listener for resetting the grid and adding new grid
 reset.addEventListener('click', () => {
   let remove = document.querySelector('#container'); //selecting id of container
   while (remove.firstChild) { //while is it true that container has a first child
     remove.removeChild(remove.firstChild); //remove child from container
   }
+  makeGrid();
 });
+
+makeRows(16, 16); //default grid
