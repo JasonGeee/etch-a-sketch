9b65ad34ab2764b12bf7f451b1cdcc460cb1cc14
Jason Gutierrez <jasongeee@yahoo.com>
2022-02-12 00:01:34 -0800

    Commited changes from newEtch branch and merging into main branch

--- script.js
+++ script.js
@@ -15,14 +15,6 @@ function makeRows(rows, cols) {
 
 //Creates a new grid on the webpage
 function makeGrid() {
-<<<<<<< HEAD
-  let newGrid = prompt("Enter a number for a new grid from 0 - 100!", 16);
-  if (newGrid > 0 && newGrid < 101) {
-    makeRows(newGrid, newGrid);
-  } else {
-      makeGrid();
-    }
-=======
   let newGrid = prompt("Enter a number for a new grid from 0 - 64!", 16);
   if (newGrid > 0 && newGrid < 64) {
     makeRows(newGrid, newGrid);
@@ -32,7 +24,6 @@ function makeGrid() {
     } while (newGrid < 1 || newGrid > 65);
     makeRows(newGrid, newGrid);
   }
->>>>>>> newEtch
 }
 
 //Event listener for hovering over grid to change color
@@ -43,22 +34,9 @@ container.addEventListener('mouseover', (e) => {
   }
 });
 
-<<<<<<< HEAD
-//Event listener for resetting the grid and adding new grid
-reset.addEventListener('click', () => {
-  let remove = document.querySelector('#container'); //selecting id of container
-  while (remove.firstChild) { //while is it true that container has a first child
-    remove.removeChild(remove.firstChild); //remove child from container
-  }
-  makeGrid();
-});
-
-makeRows(16, 16); //default grid
-=======
 //Event listener for resetting the grid
 reset.addEventListener('click', () => {
   window.location.reload(); //reloads webpage to allow reset of Etch
 });
 
 makeGrid();
->>>>>>> newEtch
